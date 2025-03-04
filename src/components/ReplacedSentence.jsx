e95684a6af4c5c556aedd18af07ab615bda7e007
ramune314159265 <ramune314159265@gmail.com>
2025-03-05 00:42:22 +0900

    キーワードがハイライトされるように

--- src/components/ReplacedSentence.jsx
+++ src/components/ReplacedSentence.jsx
@@ -1,5 +1,6 @@
 import { Tooltip } from "@/components/ui/tooltip"
 import { Box, Text } from '@chakra-ui/react'
+import { useKeywords } from '../atoms/keywords'
 import { useReplacements } from '../atoms/replacements'
 import { useOriginalSentence } from '../atoms/sentence'
 import { splitByKeywords } from '../utils/split'
@@ -7,8 +8,9 @@ import { splitByKeywords } from '../utils/split'
 export const ReplacedSentence = () => {
 	const [originalSentence] = useOriginalSentence()
 	const [replacements] = useReplacements()
+	const [keywords] = useKeywords()
 	const replacementsFrom = replacements.map(e => e.from)
-	const splitted = splitByKeywords(originalSentence, replacementsFrom)
+	const splitted = splitByKeywords(originalSentence, [...replacementsFrom, ...keywords])
 	return (
 		<Box
 			paddingInline="3"
@@ -33,6 +35,18 @@ export const ReplacedSentence = () => {
 										color="green.500"
 									>
 										{to}
+									</Text>
+								</Tooltip>
+							)
+						}
+						case keywords.includes(i): {
+							return (
+								<Tooltip key={index} content={`キーワード: ${i}`} showArrow openDelay="0" closeDelay="0">
+									<Text
+										as="span"
+										color="yellow.500"
+									>
+										{i}
 									</Text>
 								</Tooltip>
 							)
